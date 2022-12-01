7114f2b2a27f11e076f44edf33bafd44e8af1d8a
Sajith Muralidhar <89123444+Section141@users.noreply.github.com>
2022-12-01 17:36:33 -0600

    Project Update

--- select_repos/dev_count.py
+++ select_repos/dev_count.py
@@ -8,6 +8,10 @@ import subprocess
 import shutil
 from git import Repo
 from git import exc 
+import logging
+
+logging.basicConfig(filename="logger.log", level=logging.DEBUG)
+logger = logging.getLogger("dev_count")
 
 
 def giveTimeStamp():
@@ -17,49 +21,52 @@ def giveTimeStamp():
 
     
 def getDevEmailForCommit(repo_path_param, hash_):
-    author_emails = []
-
-    cdCommand         = "cd " + repo_path_param + " ; "
-    commitCountCmd    = " git log --format='%ae'" + hash_ + "^!"
-    command2Run = cdCommand + commitCountCmd
-
-    author_emails = str(subprocess.check_output(['bash','-c', command2Run]))
-    author_emails = author_emails.split('\n')
-    author_emails = [x_.replace(hash_, '') for x_ in author_emails if x_ != '\n' and '@' in x_ ] 
-    author_emails = [x_.replace('^', '') for x_ in author_emails if x_ != '\n' and '@' in x_ ] 
-    author_emails = [x_.replace('!', '') for x_ in author_emails if x_ != '\n' and '@' in x_ ] 
-    author_emails = [x_.replace('\\n', ',') for x_ in author_emails if x_ != '\n' and '@' in x_ ] 
+    logger.info(f"getDevEmailForCommit({repo_path_param}, {hash_})")
     try:
-        author_emails = author_emails[0].split(',')
-        author_emails = [x_ for x_ in author_emails if len(x_) > 3 ] 
-        author_emails = list(np.unique(author_emails) )
-    except IndexError as e_:
-        pass
-    return author_emails  
-    
+     author_emails = []
+     cdCommand         = "cd " + repo_path_param + " ; "
+     commitCountCmd    = " git log --format='%ae'" + hash_ + "^!"
+     command2Run = cdCommand + commitCountCmd
+     author_emails = str(subprocess.check_output(['bash','-c', command2Run]))
+     author_emails = author_emails.split('\n')
+     author_emails = [x_.replace(hash_, '') for x_ in author_emails if x_ != '\n' and '@' in x_ ] 
+     author_emails = [x_.replace('^', '') for x_ in author_emails if x_ != '\n' and '@' in x_ ] 
+     author_emails = [x_.replace('!', '') for x_ in author_emails if x_ != '\n' and '@' in x_ ] 
+     author_emails = [x_.replace('\\n', ',') for x_ in author_emails if x_ != '\n' and '@' in x_ ] 
+     try:
+         author_emails = author_emails[0].split(',')
+         author_emails = [x_ for x_ in author_emails if len(x_) > 3 ] 
+         author_emails = list(np.unique(author_emails) )
+     except IndexError as e_:
+         pass
+     return author_emails  
+    except Exception:
+        logger.error(f"getDevEmailForCommit({repo_path_param}, {hash_}) FAILED {Exception}")
+        raise Exception
     
 def getDevCount(full_path_to_repo, branchName='master', explore=1000):
-    repo_emails = []
-    if os.path.exists(full_path_to_repo):
-        try:
-            repo_  = Repo(full_path_to_repo, search_parent_directories=True)
-            try:
-                all_commits = list(repo_.iter_commits(branchName))   
-
-                for commit_ in all_commits:
-                    commit_hash = commit_.hexsha
-                    emails = getDevEmailForCommit(full_path_to_repo, commit_hash)
-                    repo_emails = repo_emails + emails
-                
-            except exc.GitCommandError:
-                print('Skipping this repo ... due to branch name problem', full_path_to_repo )
-        except exc.InvalidGitRepositoryError:
-            print('Skipping this repo ... due to not finding the git repo', full_path_to_repo )
-    else:
-        repo_emails = [ str(x_) for x_ in range(10) ]
-    
-    return len(repo_emails)
-            
+    logger.info(f"getDevCount({full_path_to_repo}, {branchName},{explore})")
+    try:
+     repo_emails = []
+     if os.path.exists(full_path_to_repo):
+         try:
+             repo_  = Repo(full_path_to_repo, search_parent_directories=True)
+             try:
+                 all_commits = list(repo_.iter_commits(branchName))
+                 for commit_ in all_commits:
+                     commit_hash = commit_.hexsha
+                     emails = getDevEmailForCommit(full_path_to_repo, commit_hash)
+                     repo_emails = repo_emails + emails
+             except exc.GitCommandError:
+                 print('Skipping this repo ... due to branch name problem', full_path_to_repo )
+         except exc.InvalidGitRepositoryError:
+             print('Skipping this repo ... due to not finding the git repo', full_path_to_repo )
+     else:
+         repo_emails = [ str(x_) for x_ in range(10) ]
+     return len(repo_emails)
+    except Exception:
+        logger.error(f"getDevCount({full_path_to_repo},{branchName},{explore}) FAILED {Exception}")
+        raise Exception        
             
 def getTopRepos(repo_list): 
     repo_dev_dict = {}
@@ -98,4 +105,4 @@ if __name__=='__main__':
     t2 = time.time()
     time_diff = round( (t2 - t1 ) / 60, 5) 
     print('Duration: {} minutes'.format(time_diff) )
-    print( '*'*100  )  
+    print( '*'*100  )   
